ad498498891b42984208f18435dcdd8e522eaa79
chinen-eita-FTC <e_chinen@ft-cons.co.jp>
2019-09-28 15:17:09 +0000

    Javadocのサンプルを追加

--- src/main/java/jp/co/flm/app/UserController.java
+++ src/main/java/jp/co/flm/app/UserController.java
@@ -6,8 +6,15 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * コメントサンプル
+ */
 @RestController
 public class UserController {
+	/**
+	 * コメントサンプル
+	 * @return JSON形式のデータを返却予定
+	 */
 	@RequestMapping("/user")
 	public List<String> test() {
 		return Arrays.asList("未実装", "未実装", "未実装");
