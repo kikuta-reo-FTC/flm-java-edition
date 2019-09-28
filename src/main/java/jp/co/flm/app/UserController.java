668e737c5eedca0f20ad7c90aaccca1ba6c1fc4c
chinen-eita-FTC <e_chinen@ft-cons.co.jp>
2019-09-28 14:44:34 +0000

    コントローラの不要な処理を削除

--- src/main/java/jp/co/flm/app/UserController.java
+++ src/main/java/jp/co/flm/app/UserController.java
@@ -1,16 +1,12 @@
 package jp.co.flm.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jp.co.flm.domain.service.ManageBookService;
-
 @RestController
 public class UserController {
-	@Autowired ManageBookService manageBookService;
 	@RequestMapping("/user")
 	public String test() {
-		return "未実装です。" + manageBookService.getName();
+		return "未実装です。";
 	}
 }
