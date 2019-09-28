e9196ce9b1850762a475d613e732f841a1d5a57b
chinen-eita-FTC <e_chinen@ft-cons.co.jp>
2019-09-28 14:55:07 +0000

    コントローラのレスポンスの形式を変更

--- src/main/java/jp/co/flm/app/UserController.java
+++ src/main/java/jp/co/flm/app/UserController.java
@@ -1,12 +1,15 @@
 package jp.co.flm.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 	@RequestMapping("/user")
-	public String test() {
-		return "未実装です。";
+	public List<String> test() {
+		return Arrays.asList("未実装", "未実装", "未実装");
 	}
 }
